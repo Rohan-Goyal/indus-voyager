4fc3585494ce7b4dcded877a47282cba56d67a80
Rohan <goyal.rohan.03@gmail.com>
2021-06-15 13:33:50 +0400

    Introduced TCMs

--- poliastro-voyager.py
+++ poliastro-voyager.py
@@ -18,22 +18,78 @@ def magnitude(obj):
     return (norm(obj.rv()[0]).to(u.AU), norm(obj.rv()[1]))
 
 
-def assist_to_planet(
-    planet,
-    target_date,
-    current_orbit,
-    color="black",
-    anom=(0, 180),
-    impulses=[],
-):
+def tcm():
+
+    t = Time
+    m = Maneuver.impulse
+
+    # Maneuvers
+    # Hashtable, with names TCM1, TCM2, etc. Each value is a maneuver with a time specified as an astropy epoch, and a vector of delta-V in
+    return {
+        # "01":m(()),
+        # "02":m(()),
+        # "03":m(()),
+        "04": (
+            m([563.738, 522.016, 29.424] * u.mm / u.s),
+            t("1979-06-27 10:09:09", scale="utc").tdb,
+        ),
+        "05": (
+            m([8723.415, -8807.558, -3908.574] * u.mm / u.s),
+            t("1979-07-10 00:39:39", scale="utc").tdb,
+        ),
+        "06": (
+            m([-166.348, 607.505, 500.864] * u.mm / u.s),
+            t("1979-07-23 16:09:50", scale="utc").tdb,
+        ),
+        # "07":m(()),
+        "08": (
+            m([-469.649, 882.511, 648.463] * u.mm / u.s),
+            t("1981-07-19 11:16:25", scale="utc").tdb,
+        ),
+        "09": (
+            m([-230.733, 1205.817, -534.300] * u.mm / u.s),
+            t("1981-08-18 21:26:16", scale="utc").tdb,
+        ),
+        # "10":m(()),
+        # "11":m(()),
+        # "12":m(()),
+        # "13":m(()),
+        # "14":m(()),
+        # "15":m(()),
+        # "16":m(()),
+        "17": (
+            m([91.925, -339.117, -10.428] * u.mm / u.s),
+            t("1989-04-20 16:19:46", scale="utc").tdb,
+        ),
+        "18": (
+            m([-335.093, -850.755, -161.348] * u.mm / u.s),
+            t("1989-08-01 12:55:18", scale="utc").tdb,
+        ),
+        # "19":m(()), # Cancelled
+        "20": (
+            m([-477.544, -8.441, -12.920] * u.mm / u.s),
+            t("1989-08-21 12:48:06", scale="utc").tdb,
+        ),
+    }
+
+
+def impulses(date, current, mans=tcm()):
+    # Get all impulses between end of current orbit and target dat
+    return [i for i in mans.values() if current.epoch < i[1] < date]
+
+
+def assist_to_planet(planet, arrival, current, color, anom):
     planet_orbit = Ephem.from_body(
-        planet, time_range(launch_date, end=target_date + TimeDelta(1 * u.yr))
+        planet, time_range(launch_date, end=arrival + TimeDelta(1 * u.yr))
     )
-    ss = Orbit.from_ephem(Sun, planet_orbit, target_date)
-    man = Maneuver.lambert(current_orbit, ss)
-    flyby, target = current_orbit.apply_maneuver(man, intermediate=True)
-    flyby_end = flyby.propagate(target_date + TimeDelta(0 * u.wk))
-    plotter.plot_body_orbit(planet, target_date, label=f"{planet} End of Flyby")
+    ss = Orbit.from_ephem(Sun, planet_orbit, arrival)
+    man = Maneuver.lambert(current, ss)
+    flyby, target = current.apply_maneuver(man, intermediate=True)
+    for i in impulses(arrival, current):
+        flyby.propagate(i[1])
+        (flyby,) = flyby.apply_maneuver(i[0], intermediate=True)
+    flyby_end = flyby.propagate(arrival + TimeDelta(0 * u.wk))
+    plotter.plot_body_orbit(planet, arrival, label=f"{planet} End of Flyby")
     plotter.plot_trajectory(
         flyby_end.sample(min_anomaly=anom[0] * u.deg, max_anomaly=anom[1] * u.deg),
         label=f"To {planet}",
@@ -55,33 +111,7 @@ uranus_date = Time("1986-01-24", scale="utc").tdb
 neptune_date = Time("1989-08-25", scale="utc").tdb
 
 earth = Ephem.from_body(Earth, time_range(launch_date, end=saturn_date))
-t = Time
-m = Maneuver.impulse
 
-# Maneuvers
-# Hashtable, with names TCM1, TCM2, etc. Each value is a maneuver with a time specified as an astropy epoch, and a vector of delta-V in
-tcm = {
-    # "01":m(()),
-    # "02":m(()),
-    # "03":m(()),
-    "04": (m([563.738, 522.016, 29.424] * u.mm / u.s), t("1979-06-27 10:09:09")),
-    "05": (m([8723.415, -8807.558, -3908.574] * u.mm / u.s), t("1979-07-10 00:39:39")),
-    "06": (m([-166.348, 607.505, 500.864] * u.mm / u.s), t("1979-07-23 16:09:50")),
-    # "07":m(()),
-    "08": (m([-469.649, 882.511, 648.463] * u.mm / u.s), t("1981-07-19 11:16:25")),
-    "09": (m([-230.733, 1205.817, -534.300] * u.mm / u.s), t("1981-08-18 21:26:16")),
-    # "10":m(()),
-    # "11":m(()),
-    # "12":m(()),
-    # "13":m(()),
-    # "14":m(()),
-    # "15":m(()),
-    # "16":m(()),
-    "17": (m([91.925, -339.117, -10.428] * u.mm / u.s), t("1989-04-20 16:19:46")),
-    "18": (m([-335.093, -850.755, -161.348] * u.mm / u.s), t("1989-08-01 12:55:18")),
-    # "19":m(()), # Cancelled
-    "20": (m([-477.544, -8.441, -12.920] * u.mm / u.s), t("1989-08-21 12:48:06")),
-}
 
 ss_e0 = Orbit.from_ephem(Sun, earth, launch_date)
 ss_e0_end = ss_e0.propagate_to_anomaly(180.0 * u.deg)
@@ -107,16 +137,16 @@ plotter.plot_body_orbit(Earth, ss_e0_end.epoch, label=f"{Earth} at end of flyby"
 # I imagine we could specify target orbits rather than simply target points based on ephemeris files,
 # but that doesn't work for the next mission.
 jupiter_end, jupiter_cost = assist_to_planet(
-    Jupiter, jupiter_date, init_orbit_end, anom=(0, 150)
+    Jupiter, jupiter_date, init_orbit_end, "black", (0, 150)
 )
 saturn_end, saturn_cost = assist_to_planet(
-    Saturn, saturn_date, jupiter_end, color="blue", anom=(30, 80)
+    Saturn, saturn_date, jupiter_end, "blue", (30, 80)
 )
 uranus_end, uranus_cost = assist_to_planet(
-    Uranus, uranus_date, saturn_end, color="red", anom=(5, 70)
+    Uranus, uranus_date, saturn_end, "red", (5, 70)
 )
 neptune_end, neptune_cost = assist_to_planet(
-    Neptune, neptune_date, uranus_end, color="green", anom=(40, 70)
+    Neptune, neptune_date, uranus_end, "green", (40, 70)
 )
 
 vectors = [
@@ -128,7 +158,7 @@ vectors = [
 ]
 
 pprint(vectors)
-pprint(f"Jupiter: {jupiter_cost} \n")
-pprint(f"Saturn: {saturn_cost} \n")
-pprint(f"Uranus: {uranus_cost} \n")
-pprint(f"Neptune: {neptune_cost} \n")
+pprint(f"Jupiter: {jupiter_cost}")
+pprint(f"Saturn: {saturn_cost}")
+pprint(f"Uranus: {uranus_cost}")
+pprint(f"Neptune: {neptune_cost}")
