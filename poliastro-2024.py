c81ac4105e726199345a2281190850f5827f59bf
Rohan Goyal <rohangoyal@Ashishs-MacBook-Pro.local>
2021-06-21 09:07:51 +0400

    Added patched conic discussion

--- poliastro-2024.py
+++ poliastro-2024.py
@@ -8,71 +8,75 @@ from poliastro.ephem import Ephem
 from poliastro.maneuver import Maneuver
 from poliastro.plotting import StaticOrbitPlotter
 from poliastro.twobody import Orbit
-from poliastro.util import time_range
+from poliastro.util import time_range, norm
 
 solar_system_ephemeris.set("jpl")
+plotter = StaticOrbitPlotter()
 
-# TODO
-launch_date = Time("2024-08-20 14:29", scale="utc").tdb
-jupiter_date = Time("2026-07-09 22:29", scale="utc").tdb
-uranus_date = Time("2033-01-24", scale="utc").tdb
-neptune_date = Time("2036-08-25", scale="utc").tdb
+def magnitude(obj):
+    return (norm(obj.rv()[0]).to(u.AU), norm(obj.rv()[1]))
 
 
-c_3 = 102.4 * u.km ** 2 / u.s ** 2
-
-earth = Ephem.from_body(Earth, time_range(launch_date, end=jupiter_date))
-
-
-def assist_to_planet(
-    planet,
-    target_date,
-    current_orbit,
-    next_date,
-    color="C1",
-    delta=TimeDelta(0 * 60, format="sec"),
-    anom=(0, 180),
-):
-    planet_orbit = Ephem.from_body(planet, time_range(launch_date, end=next_date))
-    ss = Orbit.from_ephem(Sun, planet_orbit, target_date)
-    man = Maneuver.lambert(current_orbit, ss)
-    flyby, target = current_orbit.apply_maneuver(man, intermediate=True)
-    flyby_end = flyby.propagate(target_date + delta)
-    plotter.plot_body_orbit(planet, target_date, label=f"{planet} End of Flyby")
+def assist_to_planet(planet, arrival, current, color, anom):
+    planet_orbit = Ephem.from_body(
+        planet, time_range(launch_date, end=arrival + TimeDelta(1 * u.yr))
+    )
+    ss = Orbit.from_ephem(Sun, planet_orbit, arrival)
+    man = Maneuver.lambert(current, ss)
+    flyby, target = current.apply_maneuver(man, intermediate=True)
+    flyby_end = flyby.propagate(arrival + TimeDelta(0 * u.wk))
+    plotter.plot_body_orbit(planet, arrival, label=f"{planet} End of Flyby")
     plotter.plot_trajectory(
         flyby_end.sample(min_anomaly=anom[0] * u.deg, max_anomaly=anom[1] * u.deg),
         label=f"To {planet}",
         color=color,
     )
-    return man.impulses, flyby_end, target, man.get_total_cost()
+    return flyby_end, man.get_total_cost()
 
 
-# Basically, we flyby each of the planets successively.
+launch_date = Time("2024-08-20 14:29", scale="utc").tdb
+jupiter_date = Time("2026-07-09 22:29", scale="utc").tdb
+uranus_date = Time("2033-01-24", scale="utc").tdb
+neptune_date = Time("2036-08-25", scale="utc").tdb
 
+earth = Ephem.from_body(Earth, time_range(launch_date, end=jupiter_date))
 # DONE: Initial launch stuff, and define initial orbit by applying a launch delta-V maneuver to the initial Earth orbit.
 ss_e0 = Orbit.from_ephem(Sun, earth, launch_date)
 ss_e0_end = ss_e0.propagate_to_anomaly(180.0 * u.deg)
-init_orbit=Orbit.geostationary(Earth, period=24*u.hour)
+# init_orbit = Orbit.synchronous(Earth)
+
+init_orbit = Orbit.from_classical(
+    Sun,
+    -3.76054 * u.AU,
+    0.733305558 * u.one,
+    4.87003 * u.deg,
+    327.8026 * u.deg,
+    11.65107 * u.deg,
+    348.80709 * u.deg,
+)
 init_orbit_end = init_orbit.propagate(
     Time("2024-12-20 14:29", scale="utc").tdb
 )  # TODO: This bit is sticky.
-plotter = StaticOrbitPlotter()
 plotter.plot_body_orbit(Earth, ss_e0_end.epoch, label=f"{Earth} at end of flyby")
-
-jupiter_impulse, jupiter_end, jupiter_target, jupiter_cost = assist_to_planet(
-    Jupiter, jupiter_date, init_orbit_end, uranus_date, anom=(0, 150)
+plotter.plot_trajectory(init_orbit.sample(min_anomaly=0*u.deg,max_anomaly=180*u.deg))
+jupiter_end, jupiter_cost = assist_to_planet(
+    Jupiter, jupiter_date, init_orbit_end, "black", (0,163)
 )
+uranus_end, uranus_cost = assist_to_planet(
+    Uranus, uranus_date, jupiter_end, "red", (0, 160)
+)
+neptune_end, neptune_cost = assist_to_planet(
+    Neptune, neptune_date, uranus_end, "green", (40, 68)
+)
+
+vectors = [
+    magnitude(init_orbit),
+    magnitude(jupiter_end),
+    magnitude(uranus_end),
+    magnitude(neptune_end),
+]
+
+pprint(vectors)
 pprint(f"Jupiter: {jupiter_cost}")
-print()
-
-uranus_impulse, uranus_end, uranus_target, uranus_cost = assist_to_planet(
-    Uranus, uranus_date, jupiter_end, neptune_date, color="C3", anom=(20, 95)
-)
 pprint(f"Uranus: {uranus_cost}")
-print()
-
-neptune_impulse, neptune_end, neptune_target, neptune_cost = assist_to_planet(
-    Neptune, neptune_date, uranus_end, neptune_date, color="C4", anom=(80, 120)
-)
 pprint(f"Neptune: {neptune_cost}")
-print()
--- pykep-voyager.py
+++ pykep-voyager.py
@@ -90,25 +90,22 @@ def modern():
     udp.plot(archi.get_champions_x()[idx])
 
 def modern_unpowered():
-
-    # We define an Earth-Venus-Earth problem (single-objective)
     seq = [jpl_lp('earth'), jpl_lp('jupiter'), jpl_lp('uranus'),jpl_lp('neptune')]
     udp = mga(
         seq=seq,
         t0=[epoch_from_string("2024-01-01 00:00:00"), epoch_from_string("2025-12-31 23:59:59")],
         tof=[[y,3*y], [4*y,8*y], [2.5*y,4.5*y]], # TODO: Lower bounds
-        vinf=[35,40],
+        vinf=40,
+        tof_encoding="direct"
     )
-
     pg.problem(udp)
     # We solve it!!
-    uda = pg.sade(gen=120)
-    archi = pg.archipelago(algo=uda, prob=udp, n=8, pop_size=20)
-    print(
-        "Running a Self-Adaptive Differential Evolution Algorithm .... on 8 parallel islands")
-    archi.evolve(12)
+    uda = pg.sade(gen=100)
+    archi = pg.archipelago(algo=uda, prob=udp, n=10, pop_size=25)
+    archi.evolve(15)
     archi.wait()
     sols = archi.get_champions_f()
+    print(sols)
     idx = sols.index(min(sols))
     print("Done!! Solutions found are: ", archi.get_champions_f())
     udp.pretty(archi.get_champions_x()[idx])
